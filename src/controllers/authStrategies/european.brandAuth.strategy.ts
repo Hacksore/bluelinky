1b5cc574aeb982e492e9bac0682a57d374a9b7ce
neoPix <david@balandavid.com>
2021-04-13 12:02:45 +0200

    feat(eu): managing got 302

--- src/controllers/authStrategies/european.brandAuth.strategy.ts
+++ src/controllers/authStrategies/european.brandAuth.strategy.ts
@@ -8,6 +8,15 @@ const stdHeaders = {
 	'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_1 like Mac OS X) AppleWebKit/604.3.5 (KHTML, like Gecko) Version/11.0 Mobile/15B92 Safari/604.1'
 };
 
+const manageGot302 = <T extends Buffer | string | Record<string, unknown>>(got: Promise<got.Response<T>>): Promise<got.Response<T>> => {
+	return got.catch((error) => {
+		if (error.name === 'HTTPError' && error.statusCode === 302) {
+			return error.response;
+		}
+		return Promise.reject(error);
+	});
+};
+
 export class EuropeanBrandAuthStrategy implements AuthStrategy {
 	constructor(private readonly environment: EuropeanBrandEnvironment, private readonly language: EULanguages) { }
 
@@ -31,14 +40,14 @@ export class EuropeanBrandAuthStrategy implements AuthStrategy {
 		if (!preparedUrl) {
 			throw new Error('@EuropeanBrandAuthStrategy.login: cannot found the auth url from the form.');
 		}
-		const { headers: { location: redirectTo }, body } = await got.post(preparedUrl, {
+		const { headers: { location: redirectTo }, body } = await manageGot302(got.post(preparedUrl, {
 			cookieJar,
 			body: `username=${user.username}&rememberMe=on&password=${user.password}&credentialId=`, // TODO have a better management of the query string encoding
 			headers: {
 				'Content-Type': 'application/x-www-form-urlencoded',
 				...stdHeaders
 			},
-		});
+		}));
 		if(!redirectTo) {
 			const errorMessage = /<span class="kc-feedback-text">(.+)<\/span>/gm.exec(body);
 			if (errorMessage) {
