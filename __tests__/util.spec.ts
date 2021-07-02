dfe4736d2df7b149497c26983fc000d026266d27
David Balan <david.balan@fr.clara.net>
2021-07-02 21:00:13 +0200

    fix(tempCode): fixing and testing eu temp codes

--- __tests__/util.spec.ts
+++ __tests__/util.spec.ts
@@ -5,9 +5,21 @@ describe('Utility', () => {
   it('converts temp code to celsius in CA', () => {
     expect(tempCodeToCelsius(REGIONS.CA, '06H')).toEqual(17);
   });
-
+  
   it('converts celsius to temp code in CA', () => {
     expect(celciusToTempCode(REGIONS.CA, 17)).toEqual('06H');
+  });
+
+  it('converts temp code to celsius in EU', () => {
+    expect(tempCodeToCelsius(REGIONS.EU, '06H')).toEqual(17);
+    expect(tempCodeToCelsius(REGIONS.EU, '0CH')).toEqual(20);
+    expect(tempCodeToCelsius(REGIONS.EU, '1AH')).toEqual(27);
+  });
+
+  it('converts celcius to temp code in EU', () => {
+    expect(celciusToTempCode(REGIONS.EU, 17)).toEqual('06H');
+    expect(celciusToTempCode(REGIONS.EU, 20)).toEqual('0CH');
+    expect(celciusToTempCode(REGIONS.EU, 27)).toEqual('1AH');
   });
 
   it('parseDate converts string to date', () => {
