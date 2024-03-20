fdcb668c5366dee4c3dfa65baf7ce25764775c31
D <me@kumo.dev>
2024-03-21 09:42:45 +1100

    update cfb

--- src/constants/australia.cfb.ts
+++ src/constants/australia.cfb.ts
@@ -1,11 +1,5 @@
-// Auto generated file on 2023-07-29T15:08:36.193Z
+// Auto generated file on 2024-03-20T22:41:31.183Z
 // run `npm run eu:export:cfb` or `npm run au:export:cfb` respectively to update it
 
-export const hyundaiCFB = Buffer.from(
-  'V60WkEmyRQaAfrBF1623/7QL62MjLVbCHdItGzQ1g5T/hkmKmMVTaMHv4cKGzgD3gL8=',
-  'base64'
-);
-export const kiaCFB = Buffer.from(
-  'IDbMgWBXgic4MAyMgf5PFFRAdGX5O3IyC3uvN3scCs0gDpTFDuyvBorlAH9JMM2/wMc=',
-  'base64'
-);
+export const kiaCFB = Buffer.from('IDbMgWBXgic4MAyMgf5PFFRAdGX5O3IyC3uvN3scCs0gDpTFDuyvBorlAH9JMM2/hys=', 'base64');
+export const hyundaiCFB = Buffer.from('V60WkEmyRQaAfrBF1623/7QL62MjLVbCHdItGzQ1g5T/hkmKmMVTaMHv4cKGzgD3kfc=', 'base64');
